dd1f601cd47c40e4069db2128fb55fce5c696ff7
Fridtjof Mund <2780577+fridtjof@users.noreply.github.com>
2019-05-14 17:12:52 +0200

    check that the path != null, get dirname again

--- src/system/io/log.php
+++ src/system/io/log.php
@@ -23,16 +23,20 @@ class log
         self::factoryConstruct($configSection);
         $config = $this->getPackageConfig();
         parent::__construct($config['name']);
-        $directory = parse_url($config['path'], PHP_URL_PATH);
-        if(!file_exists($directory)) {
-            $status = @mkdir($directory, 0777, true);
-            if($status === false) {
-                $config['path'] = sys_get_temp_dir() . '/mpcmf.' . posix_getpid() .'.log';
-                $this->addCritical("Log directory creation failed, use new path instead of original. New path: {$config['path']}");
+        $urlPath = parse_url($config['path'], PHP_URL_PATH);
+        if (!is_null($urlPath)) {
+            $directory = dirname($urlPath);
+            if(!file_exists($directory)) {
+                $status = @mkdir($directory, 0777, true);
+                if($status === false) {
+                    $config['path'] = sys_get_temp_dir() . '/mpcmf.' . posix_getpid() .'.log';
+                    $this->addCritical("Log directory creation failed, use new path instead of original. New path: {$config['path']}");
+                }
+            } elseif(!is_writable($directory)) {
+                @chmod($directory, 0777);
             }
-        } elseif(!is_writable($directory)) {
-            @chmod($directory, 0777);
         }
+        
         $this->pushHandler(new StreamHandler($config['path'], $config['level']));
         MPCMF_DEBUG && $this->addDebug("New log created: {$this->configSection}");
     }
