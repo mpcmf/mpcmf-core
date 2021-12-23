02fdcb22ddc101c28421788cc745ff2bb6dd3c48
Artem Muryumin <muryumin15@gmail.com>
2021-12-23 17:57:11 +0300

    [system/io] removed variable placeholder

--- src/system/io/monolog/customProcessors/mpcmfPidProcessor.php
+++ src/system/io/monolog/customProcessors/mpcmfPidProcessor.php
@@ -5,16 +5,9 @@ namespace mpcmf\system\io\monolog\customProcessors;
 class mpcmfPidProcessor
 {
 
-    private $placeholder;
-
-    public function __construct(string $placeholder = 'pid')
-    {
-        $this->placeholder = $placeholder;
-    }
-
     public function __invoke(array $record)
     {
-        $record[$this->placeholder] = getmypid();
+        $record['pid'] = getmypid();
 
         return $record;
     }
